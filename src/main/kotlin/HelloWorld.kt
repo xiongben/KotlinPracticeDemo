0dfb010fa80fe7c73f5ce8e88cd02e489c991478
kakaxi <kakaxi@gmail.com>
2022-08-30 23:47:18 +0800

    [add]

--- src/main/kotlin/HelloWorld.kt
+++ src/main/kotlin/HelloWorld.kt
@@ -1,3 +1,13 @@
 fun main() {
     println("hello,xb")
+    val list: List<Int> = asList(1,2,3)
+    println(list)
 }
+
+fun <T> asList(vararg ts: T): List<T> {
+    val result = ArrayList<T>()
+    for(t in ts)
+        result.add(t)
+    return result
+}
+
