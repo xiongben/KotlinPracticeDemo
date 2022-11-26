c1b9fef62802ee14c1d0d6f527849047d7a84d1e
kakaxi <kakaxi@gmail.com>
2022-11-26 22:08:10 +0800

    [add]

--- demo2/src/main/kotlin/com/xb/demo1/Test.kt
+++ demo2/src/main/kotlin/com/xb/demo1/Test.kt
@@ -1,5 +1,34 @@
 package com.xb.demo1
 
 fun main() {
-    println("hello world")
+    val p = Person()
+    p.name = "jack"
+    p.eat()
+}
+
+interface Study {
+    fun readBooks()
+    fun doHomework()
+}
+
+open class Person {
+    var name = ""
+    var age = 10
+
+    fun eat() {
+        println(name + " is eating,he is " + age + "years old.")
+    }
+}
+
+class Student : Person(), Study {
+    var sno = ""
+    var grade = 0
+
+    override fun readBooks() {
+        println("===read books =====")
+    }
+
+    override fun doHomework() {
+        println("====do homework====")
+    }
 }
