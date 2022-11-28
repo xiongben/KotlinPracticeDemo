070878e6878407203737843685de89067b96a845
kakaxi <kakaxi@gmail.com>
2022-11-28 22:58:56 +0800

    [add]

---
+++ demo2/src/main/kotlin/com/xb/demo1/StringDemo.kt
@@ -0,0 +1,28 @@
+package com.xb.demo1
+
+
+fun main() {
+    var count = "ABC123xyz!@#$#".lettersCount()
+    println(count)
+    val money1 = Money(5)
+    var money2 = Money(10)
+    val money3 = money1 + money2
+    println(money3.value)
+}
+
+fun String.lettersCount(): Int {
+    var count = 0
+    for (char in this) {
+        if (char.isLetter()) {
+            count++
+        }
+    }
+    return count
+}
+
+class Money(val value: Int) {
+    operator fun plus(money: Money): Money {
+        val sum = value + money.value
+        return Money(sum)
+    }
+}
