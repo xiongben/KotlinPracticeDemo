2932ceccbb68816ea6c7183535921e539c89ea06
kakaxi <kakaxi@gmail.com>
2022-11-29 22:46:58 +0800

    [add]

---
+++ demo2/src/main/kotlin/com/xb/demo1/FunctionDemo.kt
@@ -0,0 +1,20 @@
+package com.xb.demo1
+
+import java.lang.StringBuilder
+
+fun main() {
+    val list = listOf("apple", "banana", "orange")
+    val res = StringBuilder().build {
+        append("star eat:\n")
+        for (s in list) {
+            append(s).append("\n")
+        }
+        append("end it!")
+    }
+    println(res)
+}
+
+fun StringBuilder.build(block: StringBuilder.() -> Unit): StringBuilder {
+    block()
+    return this
+}
