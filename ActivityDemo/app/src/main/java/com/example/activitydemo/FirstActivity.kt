ec67eaede4c16ab83911e882c36cce0a8de594d3
kakaxi <kakaxi@gmail.com>
2022-12-03 22:47:19 +0800

    [add]

--- ActivityDemo/app/src/main/java/com/example/activitydemo/FirstActivity.kt
+++ ActivityDemo/app/src/main/java/com/example/activitydemo/FirstActivity.kt
@@ -2,6 +2,8 @@ package com.example.activitydemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.Toast
 
@@ -13,5 +15,18 @@ class FirstActivity : AppCompatActivity() {
         button1.setOnClickListener {
             Toast.makeText(this, "You click button1!!!", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.add_item -> Toast.makeText(this, "you click add!", Toast.LENGTH_SHORT).show()
+            R.id.remove_item -> Toast.makeText(this, "you click remove", Toast.LENGTH_SHORT).show()
+        }
+        return true
     }
 }
