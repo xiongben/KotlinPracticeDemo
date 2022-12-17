534bef0dc5a20930382e9bdc8ec376d68139045a
kakaxi <kakaxi@gmail.com>
2022-12-17 22:32:56 +0800

    [add]

--- ActivityDemo/app/src/main/java/com/example/activitydemo/ListDemoActivity.kt
+++ ActivityDemo/app/src/main/java/com/example/activitydemo/ListDemoActivity.kt
@@ -8,12 +8,30 @@ import android.widget.ListView
 class ListDemoActivity : AppCompatActivity() {
 
     private val listData = listOf<String>("apple","banner","pear","cherry","orange","watermelon","pear","grape","mango","pineapple")
+    private val fruitsList = mutableListOf<Fruit>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_demo)
-        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listData)
+        initFruits()
+        val adapter = FruitAdapter(this, R.layout.fruit_item, fruitsList)
+//        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listData)
         val listView: ListView = findViewById(R.id.listView)
         listView.adapter = adapter
+    }
+
+    private fun initFruits() {
+        repeat(10){
+            fruitsList.add(Fruit("apple", R.drawable.apple_pic))
+            fruitsList.add(Fruit("banana", R.drawable.banana_pic))
+            fruitsList.add(Fruit("orange", R.drawable.orange_pic))
+            fruitsList.add(Fruit("watermelon", R.drawable.watermelon_pic))
+            fruitsList.add(Fruit("Pear", R.drawable.pear_pic))
+            fruitsList.add(Fruit("Grape", R.drawable.grape_pic))
+            fruitsList.add(Fruit("Pineapple", R.drawable.pineapple_pic))
+            fruitsList.add(Fruit("Strawberry", R.drawable.strawberry_pic))
+            fruitsList.add(Fruit("Cherry", R.drawable.cherry_pic))
+            fruitsList.add(Fruit("Mango", R.drawable.mango_pic))
+        }
     }
 }
