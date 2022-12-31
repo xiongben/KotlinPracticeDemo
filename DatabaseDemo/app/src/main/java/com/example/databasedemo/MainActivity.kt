fa06122960de1029ccd16c228d48b0669e13a1f6
kakaxi <kakaxi@gmail.com>
2022-12-31 22:51:01 +0800

    [add]

--- DatabaseDemo/app/src/main/java/com/example/databasedemo/MainActivity.kt
+++ DatabaseDemo/app/src/main/java/com/example/databasedemo/MainActivity.kt
@@ -1,16 +1,88 @@
 package com.example.databasedemo
 
+import android.annotation.SuppressLint
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.core.content.contentValuesOf
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val dbHelper = MyDatabaseHelper(this, "BookStore.db", 1)
         createDatabase.setOnClickListener {
             dbHelper.writableDatabase
+        }
+        contentValuesOf("" to "")
+        addData.setOnClickListener {
+            val db = dbHelper.writableDatabase
+            val values1 = ContentValues().apply {
+                put("name", "The Da Vinci Code")
+                put("author", "Dan Brown")
+                put("pages", "454")
+                put("price", 16.96)
+            }
+            db.insert("Book", null, values1)
+            val values2 = ContentValues().apply {
+                // 开始组装第二条数据
+                put("name", "The Lost Symbol")
+                put("author", "Dan Brown")
+                put("pages", 510)
+                put("price", 19.95)
+            }
+            db.insert("Book", null, values2)
+        }
+
+        updateData.setOnClickListener {
+            val db = dbHelper.writableDatabase
+            val values = ContentValues()
+            values.put("price", 10.99)
+            val rows = db.update("Book", values, "name= ?", arrayOf("The Da Vinci Code"))
+            Toast.makeText(this, "rows is $rows", Toast.LENGTH_SHORT).show()
+        }
+
+        deleteData.setOnClickListener {
+            val db = dbHelper.writableDatabase
+            db.delete("Book", "pages > ?", arrayOf("500"))
+        }
+
+        queryData.setOnClickListener {
+            val db = dbHelper.writableDatabase
+            val cursor = db.query("Book", null, null, null, null, null, null)
+            if (cursor.moveToFirst()) {
+                do {
+                    val name = cursor.getColumnIndex("name")
+                    val author = cursor.getColumnIndex("author")
+                    val pages = cursor.getColumnIndex("pages")
+                    val price = cursor.getColumnIndex("price")
+                    Log.d("MainActivity", "book name is $name")
+                    Log.d("MainActivity", "book author is $author")
+                    Log.d("MainActivity", "book pages is $pages")
+                    Log.d("MainActivity", "book price is $price")
+                } while (cursor.moveToNext())
+            }
+            cursor.close()
+        }
+
+        replaceData.setOnClickListener {
+            val db = dbHelper.writableDatabase
+            db.beginTransaction() //开启事务
+            try {
+                db.delete("Book", null, null)
+                val values = cvOf("name" to "fffffff", "author" to "klkk", "pages" to 555, "price" to 20.11)
+                db.insert("Book", null, values)
+                db.setTransactionSuccessful()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            } finally {
+                db.endTransaction()
+            }
         }
     }
 }
