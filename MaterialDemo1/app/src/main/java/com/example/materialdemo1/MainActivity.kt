0a220a71ed2d02ad0b819a8f755b2c5108064391
kakaxi <kakaxi@gmail.com>
2023-01-07 20:53:58 +0800

    [add]

--- MaterialDemo1/app/src/main/java/com/example/materialdemo1/MainActivity.kt
+++ MaterialDemo1/app/src/main/java/com/example/materialdemo1/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.materialdemo1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +12,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        supportActionBar?.let {
+            it.setDisplayHomeAsUpEnabled(true)
+            it.setHomeAsUpIndicator(R.drawable.ic_menu)
+        }
+        navView.setCheckedItem(R.id.navCall)
+        navView.setNavigationItemSelectedListener {
+            drawerLayout.closeDrawers()
+            true
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId) {
+            R.id.backup -> Toast.makeText(this, "you clicked Backup", Toast.LENGTH_SHORT).show()
+            R.id.delete -> Toast.makeText(this, "you clicked Delete", Toast.LENGTH_SHORT).show()
+            R.id.settings -> Toast.makeText(this, "you clicked Settings", Toast.LENGTH_SHORT).show()
+        }
+        return true
     }
 }
