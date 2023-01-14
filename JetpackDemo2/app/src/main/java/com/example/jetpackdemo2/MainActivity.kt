9cbff8b7ef7d8b43f7c323519fe2910067305e14
kakaxi <kakaxi@gmail.com>
2023-01-14 22:20:42 +0800

    [add]

--- JetpackDemo2/app/src/main/java/com/example/jetpackdemo2/MainActivity.kt
+++ JetpackDemo2/app/src/main/java/com/example/jetpackdemo2/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.jetpackdemo2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.ViewModelProvider
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var viewModel: MainViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Log.d("mainactivity", MainViewModel::class.java.toString())
+        viewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(MainViewModel::class.java)
+//        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
     }
 }
---
+++ JetpackDemo2/app/src/main/java/com/example/jetpackdemo2/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.jetpackdemo2
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+    var count = 0
+}
