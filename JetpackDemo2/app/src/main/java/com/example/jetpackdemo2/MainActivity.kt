e06f1129d095349ecd289ee30f8d3771494301c9
kakaxi <kakaxi@gmail.com>
2023-01-18 23:43:38 +0800

    [add]

--- JetpackDemo2/app/src/main/java/com/example/jetpackdemo2/MainActivity.kt
+++ JetpackDemo2/app/src/main/java/com/example/jetpackdemo2/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,6 +15,15 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Log.d("mainactivity", MainViewModel::class.java.toString())
         viewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory()).get(MainViewModel::class.java)
-//        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        plusOneBtn.setOnClickListener {
+            viewModel.count++
+            refreshCounter()
+        }
+        refreshCounter()
+    }
+
+    private fun refreshCounter() {
+        infoText.text = viewModel.count.toString()
+
     }
 }
