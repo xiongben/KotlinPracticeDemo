7d3e4ebd6f86ba9bcc0be79494abea5883a7aa7b
kakaixi <kakaxi@gmail.com>
2023-02-18 22:45:41 +0800

    [add]

--- RoomDemo/app/src/main/java/com/example/roomdemo/MainActivity.kt
+++ RoomDemo/app/src/main/java/com/example/roomdemo/MainActivity.kt
@@ -3,47 +3,55 @@ package com.example.roomdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.work.OneTimeWorkRequest
+import androidx.work.WorkManager
+import com.example.roomdemo.databinding.ActivityMainBinding
 import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
-//    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        binding = ActivityMainBinding.inflate(layoutInflater)
-//        val view = binding.root
-//        setContentView(view)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-//        val userDao = AppDatabase.getDatabase(this).userDao()
-//        val user1 = User("Tom", "Brady", 40)
-//        val user2 = User("Tom","Hanks", 63)
+        val userDao = AppDatabase.getDatabase(this).userDao()
+        val user1 = User("Tom", "Brady", 40)
+        val user2 = User("Tom","Hanks", 63)
 
-//        addDataBtn.setOnClickListener {
-//            thread {
-//                user1.id = userDao.insertUser(user1)
-//                user2.id = userDao.insertUser(user2)
-//            }
-//        }
-//
-//        updateDataBtn.setOnClickListener {
-//            thread {
-//                user1.age = 42
-//                userDao.updateUser(user1)
-//            }
-//        }
-//
-//        deleteDataBtn.setOnClickListener {
-//            thread {
-//                userDao.deleteUserByLastName("Hanks")
-//            }
-//        }
-//
-//        queryDataBtn.setOnClickListener {
-//            thread {
-//                for (user in userDao.loadAllUsers()) {
-//                    Log.d("MainActivity", user.toString())
-//                }
-//            }
-//        }
+        binding.addDataBtn.setOnClickListener {
+            thread {
+                user1.id = userDao.insertUser(user1)
+                user2.id = userDao.insertUser(user2)
+            }
+        }
+
+        binding.updateDataBtn.setOnClickListener {
+            thread {
+                user1.age = 42
+                userDao.updateUser(user1)
+            }
+        }
+
+        binding.deleteDataBtn.setOnClickListener {
+            thread {
+                userDao.deleteUserByLastName("Hanks")
+            }
+        }
+
+        binding.queryDataBtn.setOnClickListener {
+            thread {
+                for (user in userDao.loadAllUsers()) {
+                    Log.d("MainActivity", user.toString())
+                }
+            }
+        }
+
+        binding.doWorkBtn.setOnClickListener {
+            val request = OneTimeWorkRequest.Builder(SimpleWorker::class.java).build()
+            WorkManager.getInstance(this).enqueue(request)
+        }
     }
 }
